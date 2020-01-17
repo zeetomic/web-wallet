6d1b0276605b8aa5a12ab73e031513d84958a24a
Piset Heang <piset2cambo@gmail.com>
2020-01-17 16:00:35 +0700

    test

--- store/index.js
+++ store/index.js
@@ -1,29 +1,30 @@
 import cookieparser from "cookieparser";
 
+let token;
 export const actions = {
-  // async nuxtServerInit({commit}, {req}) {
-  //   if (req) {
-  //     if (!req.headers.cookie) {
-  //       return;
-  //     }
-  //     const jwtCookie = req.headers.cookie
-  //       .split(";")
-  //       .find(c => c.trim().startsWith("jwt="));
-  //     if (!jwtCookie) {
-  //       return;
-  //     }
-  //     token = jwtCookie.split("=")[1];
-  //   await commit("users/setToken", token);
-  //   }
-  // }
-  async nuxtServerInit({ commit }, { req }) {
-    if (process.server && process.static) return;
-    if (!req.headers.cookie) return;
-
-    const parsed = cookieparser.parse(req.headers.cookie);
-    const accessTokenCookie = parsed.jwt;
-
-    if (!accessTokenCookie) return;
-    await commit("users/setToken", accessTokenCookie);
+  async nuxtServerInit({commit}, {req}) {
+    if (req) {
+      if (!req.headers.cookie) {
+        return;
+      }
+      const jwtCookie = req.headers.cookie
+        .split(";")
+        .find(c => c.trim().startsWith("jwt="));
+      if (!jwtCookie) {
+        return;
+      }
+      token = jwtCookie.split("=")[1];
+      await commit("users/setToken", token);
+    }
   }
+  // async nuxtServerInit({ commit }, { req }) {
+  //   if (process.server && process.static) return;
+  //   if (!req.headers.cookie) return;
+
+  //   const parsed = cookieparser.parse(req.headers.cookie);
+  //   const accessTokenCookie = parsed.jwt;
+
+  //   if (!accessTokenCookie) return;
+  //   await commit("users/setToken", accessTokenCookie);
+  // }
 }
