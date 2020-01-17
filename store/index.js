608c87f61e06017d05089056bb0dd6d08860df3e
Piset Heang <piset2cambo@gmail.com>
2020-01-17 16:52:36 +0700

    test

--- store/index.js
+++ store/index.js
@@ -1,30 +1,42 @@
 import cookieparser from "cookieparser";
 
-let token;
+// let token;
+// export const actions = {
+//   async nuxtServerInit({commit}, {req}) {
+//     if (req) {
+//       if (!req.headers.cookie) {
+//         return;
+//       }
+//       const jwtCookie = req.headers.cookie
+//         .split(";")
+//         .find(c => c.trim().startsWith("jwt="));
+//       if (!jwtCookie) {
+//         return;
+//       }
+//       token = jwtCookie.split("=")[1];
+//       await commit("users/setToken", token);
+//     }
+//   }
+//   // async nuxtServerInit({ commit }, { req }) {
+//   //   if (process.server && process.static) return;
+//   //   if (!req.headers.cookie) return;
+
+//   //   const parsed = cookieparser.parse(req.headers.cookie);
+//   //   const accessTokenCookie = parsed.jwt;
+
+//   //   if (!accessTokenCookie) return;
+//   //   await commit("users/setToken", accessTokenCookie);
+//   // }
+// }
+let token = null;
+let parsed = null;
 export const actions = {
-  async nuxtServerInit({commit}, {req}) {
-    if (req) {
-      if (!req.headers.cookie) {
-        return;
-      }
-      const jwtCookie = req.headers.cookie
-        .split(";")
-        .find(c => c.trim().startsWith("jwt="));
-      if (!jwtCookie) {
-        return;
-      }
-      token = jwtCookie.split("=")[1];
-      await commit("users/setToken", token);
+  async nuxtServerInit ({commit}, {req, res, app}) {
+    if(req.headers.cookie) {
+      parsed = cookieparser.parse(req.headers.cookie);
+      token = parsed.jwt;
+      if(!token) return;
     }
+    await commit('users/setToken', token);
   }
-  // async nuxtServerInit({ commit }, { req }) {
-  //   if (process.server && process.static) return;
-  //   if (!req.headers.cookie) return;
-
-  //   const parsed = cookieparser.parse(req.headers.cookie);
-  //   const accessTokenCookie = parsed.jwt;
-
-  //   if (!accessTokenCookie) return;
-  //   await commit("users/setToken", accessTokenCookie);
-  // }
 }
