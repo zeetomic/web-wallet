9ea0009cd3c98398f5faa69d0d6a44eeebfafec1
Piset Heang <piset2cambo@gmail.com>
2020-05-12 21:49:21 +0700

    fix bug

--- pages/successfullyregister/index.vue
+++ pages/successfullyregister/index.vue
@@ -1,20 +1,20 @@
 <template>
   <v-row class="my-container">
     <v-col class="text-center d-flex flex-column align-center justify-center">
-      <v-card class="pa-12">
-      <v-row class="d-flex justify-center pb-4">
-        <v-img :src="confirm" max-width="160px"></v-img>
-      </v-row>
-      <h2>Thank You For Register to Zeetomic Wallet</h2>
-      <br>
-      <h3>Please Verify Your Email Address</h3>
-      <p>You're almost there! we sent you an email.</p>
-      <br>
-      <span>Just click on verify button in that email to complete your signup.</span>
-      <br>
-      <span>If you don't see it, you may need to check your spam folder.</span>
-      <br>
-      <v-btn color="success" large class="mt-12" min-width="200px" to="/login">Login</v-btn>
+      <v-card class="py-12">
+        <v-row class="d-flex justify-center pb-4">
+          <v-img :src="confirm" max-width="160px"></v-img>
+        </v-row>
+        <h2>Thank You For Register to Zeetomic Wallet</h2>
+        <br>
+        <h3>Please Verify Your Email Address</h3>
+        <p>You're almost there! we sent you an email.</p>
+        <br>
+        <span>Just click on verify button in that email to complete your signup.</span>
+        <br>
+        <span>If you don't see it, you may need to check your spam folder.</span>
+        <br>
+        <v-btn color="success" large class="mt-12" min-width="200px" to="/login">Login</v-btn>
       </v-card>
     </v-col>
   </v-row>
@@ -33,6 +33,6 @@ export default {
 
 <style scoped>
 .my-container {
-  height: 100vh;
+  min-height: 100vh;
 }
 </style>
