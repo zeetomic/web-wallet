ddf4024325b0312632f4a1b2243a80b488fbd618
Piset Heang <piset2cambo@gmail.com>
2020-05-13 17:53:41 +0700

    fix bug

--- pages/receive/index.vue
+++ pages/receive/index.vue
@@ -45,7 +45,7 @@
         <v-card class="pa-2" elevation="4" v-if="history.map(h => h.amount)">
           <h2>Receive Token</h2>
           <br>
-          <HistoryMobo v-if="!history.error" :history="history.map(his => his.from !== user_profile.wallet && his )"/>
+          <HistoryMobo v-if="!history.error" :history="history.map(his => his.to === user_profile.wallet && his )"/>
           <Getwallet v-else :portfolio="history"/>
           <div 
             v-for="(item,index) in history"
